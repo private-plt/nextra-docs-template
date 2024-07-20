31390219353c1cc27c683155b60bc0f09fb6db51
plticmd <86671100+plticmd@users.noreply.github.com>
2024-07-21 06:18:23 +0900

    upload to github

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
-const config: DocsThemeConfig = {
-// logo:<b>Aptos Operating Handbook Nextra</b>,
-//   img:"/public/docs/android-chrome-192x192.png",
-  
-//   project: {
-//     link: 'https://github.com/aptos-labs/developer-docs',
-//   },
+ const config: DocsThemeConfig = {
 
   logo: (
     <>
@@ -26,8 +20,20 @@ const config: DocsThemeConfig = {
       Nextra Operating Handbook 
       </span>
     </>
-  )
+  ),
   
+  project: {
+    link: 'https://github.com/aptos-labs/developer-docs',
+    icon: (
+      <svg width="24" height="24" viewBox="0 0 256 256">
+        <path
+          fill="currentColor"
+          d="m231.9 169.8l-94.8 65.6a15.7 15.7 0 0 1-18.2 0l-94.8-65.6a16.1 16.1 0 0 1-6.4-17.3L45 50a12 12 0 0 1 22.9-1.1L88.5 104h79l20.6-55.1A12 12 0 0 1 211 50l27.3 102.5a16.1 16.1 0 0 1-6.4 17.3Z"
+        ></path>
+      </svg>
+    )
+  }
 }
 
-export default config
+ export default config
+
